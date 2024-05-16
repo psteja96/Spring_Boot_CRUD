55e59a5b27ecc2d2c60e595e317cb4e55f0afe00
psteja96 <40877713+psteja96@users.noreply.github.com>
2024-05-16 22:20:08 +0100

    final changes

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,11 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -36,5 +38,21 @@ public class StudentService {
           throw new IllegalStateException( "student"+studentId+" does not exist" );
       }
       studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId,String name,String email) {
+       Student student=studentRepository.findById(studentId).orElseThrow(()->
+               new IllegalStateException("student"+studentId+" does not exist"));
+       if(name!=null && name.length()>0 && !Objects.equals(student.getName(),name)) {
+           student.setName(name);
+       }
+        if(email!=null && email.length()>0 && !Objects.equals(student.getEmail(),email)) {
+            Optional<Student> studentOptional=studentRepository.findByStudentByEmail(student.getEmail());
+            if(studentOptional.isPresent()) {
+                throw new IllegalStateException( "email already in use" );
+            }
+            student.setEmail(email);
+        }
     }
 }
